a2234165739b5fdeab0d2b7a6167659cda9ceb1b
Buka Cakrawala <bukacakrawala@bukas-mbp.lan>
2021-09-13 14:23:31 -0700

    modify medication model

--- Model/Medicine.swift
+++ Model/Medicine.swift
@@ -7,22 +7,27 @@
 
 import Foundation
 
+struct Medicine: Identifiable {
+    let id: Int
+    let name: String
+    let type: TreatmentType
+    let remarks: String
+    let frequencies: [String]
+}
+
 enum TreatmentType {
 
+    case prescription
     case multivitamin
     case allergy
     case inhaler
-    case prescription
-    
+    case hormone
+    case pain
+    case antidepression
     case others
+    
 }
 
-struct Medicine {
-    let name: String
-    let type: TreatmentType
-    let time: Date
-    let useCount: Int
-    let description: String
-}
+
 
 
